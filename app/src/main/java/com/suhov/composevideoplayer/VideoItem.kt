14ad59edf116ddfc73da679974664348b91ca0bc
Сухов Виталий <sukhov_v@tanukitech.ru>
2022-10-24 17:55:25 +0300

    Created classes for video player

---
+++ app/src/main/java/com/suhov/composevideoplayer/VideoItem.kt
@@ -0,0 +1,10 @@
+package com.suhov.composevideoplayer
+
+import android.net.Uri
+import androidx.media3.common.MediaItem
+
+data class VideoItem(
+    val contentUri: Uri,
+    val mediaItem: MediaItem,
+    val name:String
+)

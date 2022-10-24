bd236df0d4f1e2938437e1519e2a7d1be6d27b31
Сухов Виталий <sukhov_v@tanukitech.ru>
2022-10-24 17:54:52 +0300

    Setup Hilt

---
+++ app/src/main/java/com/suhov/composevideoplayer/VideoPlayerModule.kt
@@ -0,0 +1,24 @@
+package com.suhov.composevideoplayer
+
+import android.app.Application
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object VideoPlayerModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideVideoPlayer(app: Application): Player = ExoPlayer.Builder(app).build()
+
+    @Provides
+    @ViewModelScoped
+    fun provideMetaDataReader(app: Application): MetaDataReader = MetaDataReaderImpl(app)
+
+}
